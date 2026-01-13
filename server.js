22ecadf7a0b60a0a0c414901ed05e6fc9cefde24
kuu-ag <kures96@naver.com>
2026-01-14 04:34:28 +0900

    Change font size in bigCheck class from 20px to 14px

--- server.js
+++ server.js
@@ -277,7 +277,7 @@ function layout(body, title = "데본베일 레이드 예약 사이트") {
       display:inline-flex;
       align-items:center;
       gap:6px;
-      font-size:20px;
+      font-size:14px;
     }
     .bigCheck input[type="checkbox"]{
       width:18px;
