99e47af3901550febf170d990fa54427fb168715
kuu-ag <kures96@naver.com>
2026-02-22 14:22:26 +0900

    Reduce width of upPartyCard to 120px

--- server.js
+++ server.js
@@ -617,9 +617,9 @@ function layout(body, title = "레이드 예약 사이트") {
     }
     .upPartyCard{
       position:relative;
-      width:180px;
-      flex:0 0 180px;
-      max-width:180px;
+      width:120px;
+      flex:0 0 120px;
+      max-width:120px;
       background:#020617;
       border-radius:14px;
       border:1px solid rgba(148,163,255,.35);
