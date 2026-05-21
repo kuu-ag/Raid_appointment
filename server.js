91103c4cafc26464956810f0f1e5f17f03d902fe
kuu-ag <kures96@naver.com>
2026-05-21 19:15:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2753,7 +2753,7 @@ app.post(`${ADMIN_BASE}/streamer-reserve`, requireAdmin, (req, res) => {
        confirmed, is_streamer)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0, 0, 0, 0)
   `
-  ).run(nowISO(), dateKst, raid, "streamer", "던파의 신", "던파의 신", dealer_count, buffer_count);
+  ).run(nowISO(), dateKst, raid, "streamer", "3조딜러", "3조딜러", dealer_count, buffer_count);
 
   return res.redirect(`${ADMIN_BASE}/raid`);
 });
