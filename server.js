7f62da40851b5418fc26af49294939acbcba225f
kuu-ag <kures96@naver.com>
2026-06-24 01:23:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2798,7 +2798,7 @@ app.post(`${ADMIN_BASE}/streamer-reserve`, requireAdmin, (req, res) => {
        confirmed, is_streamer)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0, 0, 0, 0)
   `
-  ).run(nowISO(), dateKst, raid, "streamer", "박종민", "박종민", dealer_count, buffer_count);
+  ).run(nowISO(), dateKst, raid, "streamer", "운없음", "운없음", dealer_count, buffer_count);
 
   return res.redirect(`${ADMIN_BASE}/raid`);
 });
